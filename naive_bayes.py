aa2ed7a1acb1ebb5328d9179b010cce289d31780
Shaishav Jogani <shaishavjogani@yahoo.in>
2018-02-19 14:13:42 -0700

    Added code explanination

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,3 +1,18 @@
+"""
+Implementation of Gaussian Naive Bayes Classification.
+
+The code is written from scratch and does NOT use any existing functions or packages which can provide the Naive Bayes
+Classifier class or fit/predict function (e.g. sklearn).
+
+I use Bank note authentication dataset, which can be downloaded from
+http://archive.ics.uci.edu/ml/datasets/banknote+authentication. Complete description of the
+dataset can be also found on that web-page.
+
+The Implementation and some of it's functions are taken from this website (https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/)
+
+Author: Shaishav Jogani
+"""
+
 import numpy as np
 from random import randrange
 import csv
